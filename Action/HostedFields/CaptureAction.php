4e5f86482e593f7b2793761104f9687c4c8c30ad
Vadim Bondarenko <v.bondarenko2991@gmail.com>
2019-06-25 12:00:09 +0300

    add tests to hosted fields capture action

--- Action/HostedFields/CaptureAction.php
+++ Action/HostedFields/CaptureAction.php
@@ -3,26 +3,16 @@
 namespace Payum\Be2Bill\Action\HostedFields;
 
 use Payum\Core\Action\ActionInterface;
-use Payum\Core\ApiAwareInterface;
-use Payum\Core\ApiAwareTrait;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayAwareInterface;
 use Payum\Core\GatewayAwareTrait;
 use Payum\Core\Request\Capture;
 use Payum\Core\Exception\RequestNotSupportedException;
-use Payum\Be2Bill\Api;
 use Payum\Be2Bill\Request\Api\ObtainCartToken;
 
-class CaptureAction implements ActionInterface, ApiAwareInterface, GatewayAwareInterface
+class CaptureAction implements ActionInterface, GatewayAwareInterface
 {
     use GatewayAwareTrait;
-    use ApiAwareTrait;
-
-    public function __construct()
-    {
-        $this->apiClass = Api::class;
-    }
-
     /**
      * {@inheritDoc}
      *
